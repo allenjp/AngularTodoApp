89836fce964efc89d51d842acd47dfde28560ef6
allenjp <jeffreypallen3@gmail.com>
2017-03-23 09:58:43 -0400

    In progress changes for adding authentication to app

--- app/routes.js
+++ app/routes.js
@@ -56,7 +56,39 @@ module.exports = function(app) {
     });
 
     // application -------------------------------------------------------------
-    app.get('*', function(req, res) {
-        res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+    app.get('/', function(req, res) {
+        // render the page and pass in any flash data if it exists
+        res.render('index.ejs', { message: req.flash('loginMessage') }); 
     });
+    
+    app.get('/todos', isLoggedIn, function(req, res) {
+        res.sendfile('./public/todos.html'); // load the single view file (angular will handle the page changes on the front-end)
+    });
+    
+    app.get('/signup', function(req, res) {
+        // render the page and pass in any flash data if it exists
+        res.render('signup.ejs', { message: req.flash('signupMessage') });
+    });
+    
+    app.get('/profile', isLoggedIn, function(req, res) {
+        res.render('profile.ejs', {
+            user : req.user // get the user out of session and pass to template
+        });
+    });
+    
+    app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+};
+    
+    // route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+
+    // if user is authenticated in the session, carry on 
+    if (req.isAuthenticated())
+        return next();
+
+    // if they aren't redirect them to the home page
+    res.redirect('/');
 }
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
-
-// set up ========================
+// set up ======================================================================
+// get all the tools we need
 var express  = require('express');
-var app      = express();                               // create our app w/ express
-var mongoose = require('mongoose');                     // mongoose for mongodb
-var morgan = require('morgan');             // log requests to the console (express4)
-var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
-var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+var app      = express();
+var port     = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
+
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
 var database = require('./config/database');
 
 // configuration =================
@@ -17,10 +23,18 @@ app.use(morgan('dev'));                                         // log every req
 app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());                                     // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-app.use(methodOverride());
+app.use(cookieParser()); // read cookies (needed for auth)
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+// required for passport
+app.use(session({ secret: 'iloveangular' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 
  // load the routes
-require('./app/routes')(app);
+require('./app/routes')(app, passport);
 
 
 // listen (start app with node server.js) ======================================
