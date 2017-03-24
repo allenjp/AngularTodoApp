fcb9063d53611e0b1132be7227ad627007cd1ad4
allenjp <jeffreypallen3@gmail.com>
2017-03-24 16:29:40 -0400

    Enabling google authentication

--- app/routes.js
+++ app/routes.js
@@ -74,6 +74,10 @@ module.exports = function(app, passport) {
         });
     });
     
+    // =====================================
+    // LOCAL ROUTES =======================
+    // =====================================
+    
     app.get('/signup', function(req, res) {
         // render the page and pass in any flash data if it exists
         res.render('signup.ejs', { message: req.flash('signupMessage') });
@@ -90,6 +94,22 @@ module.exports = function(app, passport) {
         res.redirect('/');
     });
     
+    // =====================================
+    // GOOGLE ROUTES =======================
+    // =====================================
+    // send to google to do the authentication
+    // profile gets us their basic information including their name
+    // email gets their emails
+    
+    app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
+
+    // the callback after google has authenticated the user
+    app.get('/auth/google/callback',
+            passport.authenticate('google', {
+                    successRedirect : '/todos',
+                    failureRedirect : '/'
+            }));
+
     // process the signup form
     app.post('/signup', passport.authenticate('local-signup', {
         successRedirect : '/profile', // redirect to the secure profile section
--- config/auth.js
+++ config/auth.js
@@ -1,8 +1,8 @@
 // expose our config directly to our application using module.exports
 module.exports = {
     'googleAuth' : {
-        'clientID'      : 'your-secret-clientID-here',
-        'clientSecret'  : 'your-client-secret-here',
+        'clientID'      : '935136078624-mbpcueqa9212vk9ktpkoaf9lb8sucivf.apps.googleusercontent.com',
+        'clientSecret'  : 'WFHYMadhQ_Nm8mg2m0jux2-p',
         'callbackURL'   : 'http://localhost:8080/auth/google/callback'
     }
 }
